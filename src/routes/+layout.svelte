418449b63ee187a75907d044d9c6861975aca1a4
Soeun Park <separk314@naver.com>
2024-01-20 00:43:40 +0900

    feat: Footer 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,12 @@
 <header class="text-heading1 header m-5 flex flex-row justify-between">
     <p>portfolio</p>
     <p>seomin</p>
+</header>
 
-     </header>
 <slot />
+
+<footer class="footer footer-title m-4 sticky bottom-0">
+    <p>한양대학교 짱짱맨 섬굥의 포트폴리오입니다.</p>
+    <p>Contact: 010 1234 3243</p>
+    <p>Email:   qorhvk@gmail.com</p>
+</footer>
